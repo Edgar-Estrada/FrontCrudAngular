6cc3a274a83eeace9478211cdc90d5172be37e5a
Edgar Estrada <estradacalvilloedgar@gmail.com>
2022-08-15 19:42:06 -0500

    Cambios

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,20 +1,18 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { Subject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
   public arrAlumn: any = [];
-  private alumn = new Subject<string>();
+
   constructor() {}
 
   setAlumn(alumn: any) {
     this.arrAlumn.push(alumn);
   }
 
-  getAlumn(): Observable<string> {
+  getAlumn(){
     return this.arrAlumn;
   }
 }
